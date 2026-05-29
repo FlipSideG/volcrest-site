87fbee563d739feb063933ae8e0811017fe19145
FlipSideG <rbotley@gmail.com>
2026-05-29 13:44:41 +0100

    copy: simplify homepage sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import ThreeColumns from "@/components/ThreeColumns";
-import FounderSection from "@/components/FounderSection";
-import ClosingCTA from "@/components/ClosingCTA";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -11,9 +8,6 @@ export default function Home() {
       <Navbar />
       <main className="min-h-screen bg-black">
         <Hero />
-        <ThreeColumns />
-        <FounderSection />
-        <ClosingCTA />
       </main>
       <Footer />
     </>
